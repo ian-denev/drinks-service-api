6a966af608c84fbff7a7afa3771377c246ff56c7
Ian Denev <contact@iandenev.com>
2021-11-16 12:05:11 +0000

    coffee endpoint - hardcoded

--- src/app.js
+++ src/app.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
 
 app.get("/", (req, res) => res.send("Welcome to the Drinks API!"));
 app.get("/coffeelover", (req, res) => res.send("I like coffee!"));
+app.get('/coffee', (req, res) => res.json({
+    drinkType: 'Coffee',
+    name: 'Latte',
+}));
 
 module.exports = app;
